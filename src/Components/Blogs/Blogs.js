57eb0d026e8c893d774b9053f8c6a84eeace073c
Whaeedil <baoppyhossen1234@gmail.com>
2022-04-09 17:10:32 +0600

    blogs-q-update

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import './Blogs.css'
 const Blogs = () => {
     return (
-        <div>
-            <h1>this is blogs</h1>
+        <div className='q-style'>
+            <hr />
+            <div >
+                <h3>1) what is purpose component api react ?</h3>
+                <h5>Components are reusable objects relating to various aspects  API definition. A component has no effect on the API unless it is explicitly referenced from elsewhere in  API definition.ReactJS component is a top-level API. It makes the code completely individual and reusable in the application. It includes various methods for: Creating elements. </h5>
+            </div>
+            <hr />
+            <div>
+                <h3>2) what is se</h3>
+                <h5>Semantic HTML tags provide information about the contents of those tags that goes beyond just how they look on a page. Text that is enclosed in the 'Code' tag is immediately recognized by the browser as some type of coding language.
+                    Semantic HTML elements are those that clearly describe their meaning in a human- and machine-readable way. Elements such as header , footer and article are all considered semantic because they accurately describe the purpose of the element and the type of content that is inside </h5>
+            </div>
+            <hr />
         </div>
     );
 };
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='header'>
-            <h3><small>Copyright © 2022 cartoon.com</small></h3>
+            <h3><small>Copyright © 2026 cartoon.com</small></h3>
         </div>
     );
 };
